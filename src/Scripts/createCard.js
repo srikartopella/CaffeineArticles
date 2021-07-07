feed9a0c618059066512c380cf26f1ce914619ee
Srikar Topella <srikar.topella@gmail.com>
2021-07-07 16:43:17 +0530

    Modified Index.Html File and Created createCard.js File

---
+++ src/Scripts/createCard.js
@@ -0,0 +1,42 @@
+/* 
+<!-- Card Article -->
+          <section class="col col-sm-3">
+            <div class="card p-2">
+              <img
+                class="card-image rounded"
+                src="https://images.unsplash.com/photo-1523437113738-bbd3cc89fb19?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y3NzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"
+              />
+              <h3 class="card-title my-2 fw-bold">This is card Title</h3>
+              <p>Last Updated: 12 Jan 2021</p>
+              <p class="card-text my-1">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. In
+                inventore, itaque assumenda, nisi corrupti ratione dignissimos
+                voluptate sit sint est quod eveniet.
+              </p>
+              <section class="tags my-2 d-flex">
+                <p class="tag-heading h5">Tags :</p>
+                <section class="mx-2">
+                  <a
+                    href="#"
+                    class="badge rounded-pill text-light bg-dark nav-link"
+                    >HTML</a
+                  >
+                  <a
+                    href="#"
+                    class="badge rounded-pill text-light bg-dark nav-link"
+                    >JS</a
+                  >
+                  <a
+                    href="#"
+                    class="badge rounded-pill text-light bg-dark nav-link"
+                    >CSS</a
+                  >
+                </section>
+              </section>
+              <a href="#" class="my-2 btn btn-dark btn-sm d-block">
+                View Article
+              </a>
+            </div>
+          </section>
+          <!-- Card Article -->
+*/
